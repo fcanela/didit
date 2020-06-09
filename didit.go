fbcc84ce7336acbc060815d5aa8737e0e01d6615
Francisco Canela <fcanela.dev@gmail.com>
2020-06-09 23:55:27 +0200

    feat: initial commit

---
+++ didit.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "io"
+  "os"
+  "github.com/gen2brain/beeep"
+)
+
+func main() {
+  // Redirect stdin to stdout
+  stat, _ := os.Stdin.Stat()
+  if (stat.Mode() & os.ModeCharDevice) == 0 {
+    io.Copy(os.Stdout, os.Stdin)
+  }
+
+  beeep.Notify("Hey!", "Task finished. You may want to take a look.", "assets/information.png")
+}
